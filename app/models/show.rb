1a7b26f45e29f5ecf186a412b3ed272efb3c81f0
Laura Darlak <laura@polyporedesign.com>
2018-10-12 02:17:23 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,9 +1,3 @@
-# class Show < ActiveRecord::Base
-#   has_many :characters
-#   belongs_to :actor
-#   belongs_to :network
-# end
-
 class Show < ActiveRecord::Base
   has_many :characters
   has_many   :actors, through: :characters
